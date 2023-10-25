5347f111252856fd5dd4ec987454b6cac1470393
태원 <103083087+yestw@users.noreply.github.com>
2023-10-25 14:35:14 +0900

    ➕ viewBinding 설정 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "com.example.simple_calculator"
     compileSdk = 34
-
+    buildFeatures.viewBinding=true
 
     defaultConfig {
         applicationId = "com.example.simple_calculator"
